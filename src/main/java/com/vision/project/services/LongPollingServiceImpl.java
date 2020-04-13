d78a6f3183dcca4fb2483dd7c0daaa831dae5873
ivailo aleksandrov <ivayloaleksandrov@yahoo.com>
2020-04-13 19:54:28 +0300

    changes for long polling service

--- src/main/java/com/vision/project/services/LongPollingServiceImpl.java
+++ src/main/java/com/vision/project/services/LongPollingServiceImpl.java
@@ -8,6 +8,7 @@ import com.vision.project.models.Restaurant;
 import com.vision.project.models.UserRequest;
 import com.vision.project.repositories.base.MessageRepository;
 import com.vision.project.repositories.base.OrderRepository;
+import com.vision.project.repositories.base.RestaurantRepository;
 import com.vision.project.services.base.LongPollingService;
 
 import java.time.LocalDateTime;
@@ -22,50 +23,63 @@ public class LongPollingServiceImpl implements LongPollingService {
 
     private final OrderRepository orderRepository;
     private final MessageRepository messageRepository;
+    private final RestaurantRepository restaurantRepository;
 
-    public LongPollingServiceImpl(OrderRepository orderRepository, MessageRepository messagesRepository) {
+    public LongPollingServiceImpl(OrderRepository orderRepository, MessageRepository messagesRepository, RestaurantRepository restaurantRepository) {
         this.orderRepository = orderRepository;
         this.messageRepository = messagesRepository;
+        this.restaurantRepository = restaurantRepository;
     }
 
-    public void setOrAddRequest(UserRequest newRequest){
+    public void checkRequest(UserRequest newRequest){
         int userId = newRequest.getUserId();
-        int restaurantId = newRequest.getRestaurantId();
-        LocalDateTime lastCheck = newRequest.getLastCheck();
 
         UserRequest currentRequest = userRequests.getIfPresent(userId);
         DeferredResult<UserRequestDto> waitingResult = newRequest.getRequest();
         if(currentRequest == null){
-            Restaurant restaurant = restaurantRepository.getOne(restaurantId);
-            
-            List<Message> newMessages = messageRepository.findMostRecentMessages(userId, lastCheck.toLocalDate(), lastCheck.toLocalTime());
-            List<Order> newOrders = orderRepository.findMoreRecent(lastCheck, restaurant);
-
-            if(newMessages.size() > 0 || newOrders.size() > 0) {
-                newRequest.setMessages(newMessages);
-                newRequest.setOrders(newOrders);
-
-                waitingResult.setResult(new UserRequestDto(newRequest));
-                waitingResult = null;
-            }
-            newRequest.setRequest(waitingResult);
-            userRequests.put(userId, newRequest);
-            userRequests.put(userId, newRequest);
-
+            getMoreRecent(newRequest);
         }else {
             try {
-                currentRequest.getLock().lock();
-                if (currentRequest.getDishes().size() > 0 || currentRequest.getMessages().size() > 0 || currentRequest.getOrders().size() > 0) {
-
-                    waitingResult.setResult(new UserRequestDto(currentRequest));
-                    waitingResult = null;
-                }
-                currentRequest.setRequest(waitingResult);
-                userRequests.put(userId, currentRequest);
-                restaurants.put(userId, currentRequest);
+                getDataFromRequest(currentRequest);
             }finally {
                 currentRequest.getLock().unlock();
             }
         }
+    }
+
+    private void getDataFromRequest(UserRequest currentRequest) {
+        DeferredResult<UserRequestDto> waitingResult = newRequest.getRequest();
+        int userId = currentRequest.getUserId();
+
+        currentRequest.getLock().lock();
+        if (currentRequest.getDishes().size() > 0 || currentRequest.getMessages().size() > 0 || currentRequest.getOrders().size() > 0) {
+
+            waitingResult.setResult(new UserRequestDto(currentRequest));
+            waitingResult = null;
+        }
+        currentRequest.setRequest(waitingResult);
+        userRequests.put(userId, currentRequest);
+        restaurants.put(userId, currentRequest);
+    }
+
+    private void getMoreRecent(UserRequest newRequest) {
+        DeferredResult<UserRequestDto> waitingResult = newRequest.getRequest();
+        int userId = newRequest.getUserId();
+        LocalDateTime lastCheck = newRequest.getLastCheck();
+
+        Restaurant restaurant = restaurantRepository.getOne(newRequest.getRestaurantId());
+        List<Order> newOrders = orderRepository.findMoreRecent(lastCheck, restaurant);
+        List<Message> newMessages = messageRepository.findMostRecentMessages(userId, lastCheck.toLocalDate(), lastCheck.toLocalTime());
+
+        if(newMessages.size() > 0 || newOrders.size() > 0) {
+            newRequest.setMessages(newMessages);
+            newRequest.setOrders(newOrders);
+
+            waitingResult.setResult(new UserRequestDto(newRequest));
+            waitingResult = null;
+        }
+        newRequest.setRequest(waitingResult);
+        userRequests.put(userId, newRequest);
+        userRequests.put(userId, newRequest);
     }
 }
--- src/main/java/com/vision/project/services/base/LongPollingService.java
+++ src/main/java/com/vision/project/services/base/LongPollingService.java
@@ -3,5 +3,5 @@ package com.vision.project.services.base;
 import com.vision.project.models.UserRequest;
 
 public interface LongPollingService {
-    void setOrAddRequest(UserRequest newRequest);
+    void checkRequest(UserRequest newRequest);
 }
