5ac3b66c11aef56133e6ba1dc8bf9943f0f16a74
ivailo aleksandrov <ivayloaleksandrov@yahoo.com>
2020-04-13 19:40:32 +0300

    UserRequest added

---
+++ src/main/java/com/vision/project/models/UserRequest.java
@@ -0,0 +1,88 @@
+package com.vision.project.models;
+
+import org.springframework.web.context.request.async.DeferredResult;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class UserRequest {
+    private List<Order> orders = new ArrayList<>();
+    private List<Message> messages = new ArrayList<>();
+    private List<Dish> dishes = new ArrayList<>();
+    private DeferredResult request = new DeferredResult();
+    private ReentrantLock lock = new ReentrantLock();
+    private int userId;
+    private int restaurantId;
+    private LocalDateTime lastCheck;
+
+    public UserRequest(){
+
+    }
+
+    public UserRequest(DeferredResult request){
+        this.request = request;
+    }
+
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    public List<Dish> getDishes() {
+        return dishes;
+    }
+
+    public void setDishes(List<Dish> dishes) {
+        this.dishes = dishes;
+    }
+
+    public DeferredResult getRequest() {
+        return request;
+    }
+
+    public void setRequest(DeferredResult request) {
+        this.request = request;
+    }
+
+    public ReentrantLock getLock() {
+        return lock;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(int restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
+    public LocalDateTime getLastCheck() {
+        return lastCheck;
+    }
+
+    public void setLastCheck(LocalDateTime lastCheck) {
+        this.lastCheck = lastCheck;
+    }
+}
--- src/main/java/com/vision/project/repositories/base/OrderRepository.java
+++ src/main/java/com/vision/project/repositories/base/OrderRepository.java
@@ -17,7 +17,7 @@ public interface OrderRepository extends JpaRepository<Order, Integer> {
     @Query(value = "from Order where ready = false and restaurant = :restaurant order by CASE WHEN created > updated THEN created ELSE updated END desc")
     List<Order> findByReadyFalse(@Param("restaurant")Restaurant restaurant);
 
-    @Query(value="from Order where CASE WHEN created > updated THEN created ELSE updated END > :date and restaurant = :restaurant")
+    @Query(value="from Order where CASE WHEN created > updated THEN created ELSE updated END > :date and restaurant = :restaurant order by CASE WHEN created > updated THEN created ELSE updated END desc")
     List<Order> findMoreRecent(@Param("date") LocalDateTime date, @Param("restaurant") Restaurant restaurant);
 
     @Query(value = "from Order where restaurant = :restaurant order by CASE WHEN created > updated THEN created ELSE updated END desc")
