b41387e11c1620629ed5066e71e613b97220dbdc
ivailo aleksandrov <ivayloaleksandrov@yahoo.com>
2020-07-30 23:11:59 +0300

    WebSocketInterceptor added

---
+++ src/main/java/com/vision/project/config/WebSocketInterceptor.java
@@ -0,0 +1,49 @@
+package com.vision.project.config;
+
+import com.vision.project.security.Jwt;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.List;
+import java.util.Map;
+
+public class WebSocketInterceptor implements ChannelInterceptor, HandshakeInterceptor {
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor =
+                MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+        if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+            List<String> authorization = accessor.getNativeHeader("Authorization");
+            String token = authorization.get(0);
+
+            if(token == null || !token.startsWith("Token")){
+                throw new BadCredentialsException("Jwt token is missing");
+            }
+
+            UserDetails user = Jwt.validate(token.substring(6));
+            accessor.setUser(user::getUsername);
+        }
+        return message;
+    }
+
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+        return true;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+
+    }
+}
