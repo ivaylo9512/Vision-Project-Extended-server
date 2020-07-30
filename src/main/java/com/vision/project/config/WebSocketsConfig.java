d22353745b967b5356b87b9a2ec28a8931683ba2
ivailo aleksandrov <ivayloaleksandrov@yahoo.com>
2020-07-30 23:41:41 +0300

    WebSocketsConfig added

---
+++ src/main/java/com/vision/project/config/WebSocketsConfig.java
@@ -0,0 +1,37 @@
+package com.vision.project.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketsConfig
+        implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/message", "/restaurant", "/user");
+        config.setApplicationDestinationPrefixes("/api");
+        config.setUserDestinationPrefix("/user");
+        config.setUserDestinationPrefix("/restaurant");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry
+                .addEndpoint("/api/sockets")
+                .setAllowedOrigins("*")
+                .addInterceptors(getWebSocketInterceptor());
+    }
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(getWebSocketInterceptor());
+    }
+    private WebSocketInterceptor getWebSocketInterceptor(){
+        return new WebSocketInterceptor();
+    }
+}
