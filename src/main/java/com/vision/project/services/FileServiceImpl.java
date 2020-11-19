83b13f3d95fad033647c1bbcba566823155dd8f3
ivaylo <ivayloaleksandrov@yahoo.com>
2020-11-19 23:54:43 +0200

    FileService - save and generate added

--- src/main/java/com/vision/project/services/FileServiceImpl.java
+++ src/main/java/com/vision/project/services/FileServiceImpl.java
@@ -4,16 +4,17 @@ import com.vision.project.exceptions.FileNotFoundUncheckedException;
 import com.vision.project.exceptions.FileStorageException;
 import com.vision.project.models.File;
 import com.vision.project.services.base.FileService;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Service
 public class FileServiceImpl implements FileService {
@@ -62,5 +63,16 @@ public class FileServiceImpl implements FileService {
         } catch (IOException e) {
             throw new FileStorageException("Couldn't store the image.");
         }
+    }
+
+    private void save(File image, MultipartFile receivedFile) throws IOException {
+        Path targetLocation = this.fileLocation.resolve(image.getName());
+        Files.copy(receivedFile.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    private File generate(MultipartFile receivedFile, String name) {
+        String fileType = FilenameUtils.getExtension(receivedFile.getOriginalFilename());
+        String fileName = name + "." + fileType;
+        return new File(fileName, receivedFile.getSize(), receivedFile.getContentType());
     }
 }
