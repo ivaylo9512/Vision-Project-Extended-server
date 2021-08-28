ad27d853c02fdc26765d558770b277703aa5b015
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-28 03:58:43 +0300

    FileService new implementation

--- src/main/java/com/vision/project/services/FileServiceImpl.java
+++ src/main/java/com/vision/project/services/FileServiceImpl.java
@@ -1,15 +1,19 @@
 package com.vision.project.services;
 
 import com.vision.project.exceptions.FileFormatException;
-import com.vision.project.exceptions.FileNotFoundUncheckedException;
 import com.vision.project.exceptions.FileStorageException;
+import com.vision.project.exceptions.UnauthorizedException;
 import com.vision.project.models.File;
+import com.vision.project.models.UserModel;
+import com.vision.project.repositories.base.FileRepository;
 import com.vision.project.services.base.FileService;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.persistence.EntityNotFoundException;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
@@ -20,67 +24,83 @@ import java.nio.file.StandardCopyOption;
 @Service
 public class FileServiceImpl implements FileService {
     private final Path fileLocation;
+    private final FileRepository fileRepository;
 
-    public FileServiceImpl() {
+    public FileServiceImpl(FileRepository fileRepository) {
+        this.fileRepository = fileRepository;
         this.fileLocation = Paths.get("./uploads")
                 .toAbsolutePath().normalize();
 
         try {
             Files.createDirectories(this.fileLocation);
         } catch (Exception e) {
-            throw new FileStorageException("Couldn't create directory");
+            throw new RuntimeException("Couldn't create directory");
         }
     }
 
     @Override
-    public Resource loadFileAsResource(String fileName) {
-        try {
-            Path filePath = this.fileLocation.resolve(fileName).normalize();
-            Resource resource = new UrlResource(filePath.toUri());
-            if (resource.exists()) {
-                return resource;
-            } else {
-                throw new FileNotFoundUncheckedException("File not found");
-            }
-        } catch (MalformedURLException e) {
-            throw new FileNotFoundUncheckedException("File not found " + e);
-        }
-    }
-
-    @Override
-    public File create(MultipartFile receivedFile, String name) {
-
-        File file = generate(receivedFile, name);
+    public File create(MultipartFile receivedFile, String name, String type, UserModel owner) {
+        File file = generate(receivedFile, name, type);
+        file.setOwner(owner);
 
         try {
-            if (!file.getType().startsWith("image/")) {
-                throw new FileFormatException("File should be of type IMAGE.");
-            }
-
             save(file, receivedFile);
-
-            return file;
-
         } catch (IOException e) {
             throw new FileStorageException("Couldn't store the image.");
         }
+
+        return fileRepository.save(file);
     }
 
     @Override
-    public File update(MultipartFile file, String name, long id){
-        File updatedFile = create(file, name);
-        updatedFile.setId(id);
-        return fileRepository.save(updatedFile);
+    public File update(MultipartFile receivedFile, String name, long id, String type){
+        File file = generate(receivedFile, name, type);
+        file.setId(id);
+
+        try {
+            save(file, receivedFile);
+        } catch (IOException e) {
+            throw new FileStorageException("Couldn't store the image.");
+        }
+
+        return fileRepository.save(file);
     }
 
-    private void save(File image, MultipartFile receivedFile) throws IOException {
+    @Override
+    public Resource getAsResource(String fileName){
+        try {
+            Path filePath = this.fileLocation.resolve(fileName).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+
+            if (!resource.exists()) {
+                throw new EntityNotFoundException("File not found");
+            }
+
+            return resource;
+        } catch (MalformedURLException e) {
+            throw new FileFormatException(e.getMessage());
+        }
+    }
+
+    @Override
+    public File findByName(String fileName){
+        return fileRepository.findByName(fileName);
+    }
+
+    public void save(File image, MultipartFile receivedFile) throws IOException {
         Path targetLocation = this.fileLocation.resolve(image.getName());
         Files.copy(receivedFile.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
     }
 
-    private File generate(MultipartFile receivedFile, String name) {
+    public File generate(MultipartFile receivedFile, String name, String type) {
         String fileType = FilenameUtils.getExtension(receivedFile.getOriginalFilename());
         String fileName = name + "." + fileType;
-        return new File(fileName, receivedFile.getSize(), receivedFile.getContentType());
+        File file = new File(fileName, receivedFile.getSize(), receivedFile.getContentType());
+
+        if (!file.getType().startsWith(type)) {
+            throw new FileFormatException("File should be of type " + type);
+        }
+
+        return file;
     }
 }
--- src/main/java/com/vision/project/services/base/FileService.java
+++ src/main/java/com/vision/project/services/base/FileService.java
@@ -1,13 +1,16 @@
 package com.vision.project.services.base;
 
 import com.vision.project.models.File;
+import com.vision.project.models.UserModel;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
-    Resource loadFileAsResource(String fileName);
+    Resource getAsResource(String fileName);
 
-    File update(MultipartFile file, String name, long id);
+    File update(MultipartFile file, String name, long id, String type);
 
-    File create(MultipartFile receivedFile, String name);
+    File create(MultipartFile receivedFile, String name, String type, UserModel owner);
+
+    File findByName(String fileName);
 }
