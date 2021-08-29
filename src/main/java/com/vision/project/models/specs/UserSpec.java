0aa131c8026ad891e6b17192f1a529b0408273a1
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-29 04:05:03 +0300

    UserSpec added

---
+++ src/main/java/com/vision/project/models/specs/UserSpec.java
@@ -0,0 +1,71 @@
+package com.vision.project.models.specs;
+
+public class UserSpec {
+    private int id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String country;
+
+    public UserSpec() {
+
+    }
+
+    public UserSpec(int id, String username, String firstName, String lastName, int age, String country) {
+        this.id = id;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.country = country;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
