3a464c22c9e6995ac0ab3ca56ee4d714bc358fdc
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-10-05 16:26:46 +0300

    EmailTokenService tests added

--- src/main/java/com/vision/project/services/base/EmailTokenService.java
+++ src/main/java/com/vision/project/services/base/EmailTokenService.java
@@ -5,9 +5,9 @@ import com.vision.project.models.UserModel;
 import javax.mail.MessagingException;
 
 public interface EmailTokenService {
-    void createVerificationToken(UserModel user, String token);
+    void create(EmailToken token);
 
-    EmailToken getToken(String token);
+    EmailToken findByToken(String token);
 
     void delete(EmailToken token);
 
