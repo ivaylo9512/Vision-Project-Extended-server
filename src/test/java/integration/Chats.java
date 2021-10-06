2138b1564148479c72bd09df19edee5027688a16
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-10-06 23:58:45 +0300

    Chats integration added

---
+++ src/test/java/integration/Chats.java
@@ -0,0 +1,136 @@
+package integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vision.project.config.AppConfig;
+import com.vision.project.config.SecurityConfig;
+import com.vision.project.config.TestWebConfig;
+import com.vision.project.controllers.ChatController;
+import com.vision.project.models.UserDetails;
+import com.vision.project.models.UserModel;
+import com.vision.project.security.Jwt;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.ServletContext;
+import javax.sql.DataSource;
+import javax.transaction.Transactional;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(SpringExtension.class)
+@AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration(classes = { AppConfig.class, TestWebConfig.class, SecurityConfig.class })
+@WebAppConfiguration(value = "src/main/java/com/vision/project")
+@WebMvcTest(ChatController.class)
+@Import(SecurityConfig.class)
+@ActiveProfiles("test")
+@Transactional
+public class Chats {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private DataSource dataSource;
+
+    private MockMvc mockMvc;
+    private static String adminToken, userToken;
+    private ObjectMapper objectMapper;
+
+    @BeforeAll
+    public void setup() {
+        UserModel admin = new UserModel("adminUser", "password", "ROLE_ADMIN");
+        admin.setId(1);
+
+        UserModel user = new UserModel("testUser", "password", "ROLE_USER");
+        user.setId(3);
+
+        adminToken = "Token " + Jwt.generate(new UserDetails(admin, Collections
+                .singletonList(new SimpleGrantedAuthority("ROLE_ADMIN"))));
+
+        userToken = "Token " + Jwt.generate(new UserDetails(user, Collections
+                .singletonList(new SimpleGrantedAuthority("ROLE_USER"))));
+
+        objectMapper = new ObjectMapper();
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();
+    }
+
+    @Test
+    public void assertConfig_assertRestaurantController() {
+        ServletContext servletContext = webApplicationContext.getServletContext();
+
+        assertNotNull(servletContext);
+        assertTrue(servletContext instanceof MockServletContext);
+        assertNotNull(webApplicationContext.getBean("chatController"));
+    }
+
+    @Test
+    void getChats_WithoutToken_Unauthorized() throws Exception{
+        mockMvc.perform(get("/api/chats/auth/getChats"))
+                .andExpect(status().isUnauthorized())
+                .andExpect(content().string("Jwt token is missing"));
+    }
+
+    @Test
+    void getChats_WithIncorrectToken_Unauthorized() throws Exception{
+        mockMvc.perform(get("/api/chats/auth/getChats")
+                .header("Authorization", "Token incorrect"))
+                .andExpect(status().isUnauthorized())
+                .andExpect(content().string("Jwt token is incorrect"));
+    }
+
+    @Test
+    void getSessions_WithoutToken_Unauthorized() throws Exception{
+        mockMvc.perform(get("/api/chats/auth/getSessions"))
+                .andExpect(status().isUnauthorized())
+                .andExpect(content().string("Jwt token is missing"));
+    }
+
+    @Test
+    void getSessions_WithIncorrectToken_Unauthorized() throws Exception{
+        mockMvc.perform(get("/api/chats/auth/getSessions")
+                        .header("Authorization", "Token incorrect"))
+                .andExpect(status().isUnauthorized())
+                .andExpect(content().string("Jwt token is incorrect"));
+    }
+
+    @Test
+    void newMessage_WithoutToken_Unauthorized() throws Exception{
+        mockMvc.perform(post("/api/chats/auth/newMessage"))
+                .andExpect(status().isUnauthorized())
+                .andExpect(content().string("Jwt token is missing"));
+    }
+
+    @Test
+    void newMessage_WithIncorrectToken_Unauthorized() throws Exception{
+        mockMvc.perform(post("/api/chats/auth/newMessage")
+                        .header("Authorization", "Token incorrect"))
+                .andExpect(status().isUnauthorized())
+                .andExpect(content().string("Jwt token is incorrect"));
+    }
+}
