c7341d2481b53a7b5e85f207c49f772a0e3966cb
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-10-10 23:19:44 +0300

    WrongRestaurantException removed

--- src/main/java/com/vision/project/exceptions/WrongRestaurantException.java
+++
@@ -1,8 +0,0 @@
-package com.vision.project.exceptions;
-
-public class WrongRestaurantException extends RuntimeException{
-    public WrongRestaurantException(String exception) {
-        super(exception);
-    }
-
-}
